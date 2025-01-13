a5887d64cec901ef404ae38c99d88a910875ada4
chamath lakmuthu <chamath@longwapps.com>
2025-01-13 10:13:34 +0530

    show changes ui

--- background.js
+++ background.js
@@ -2,11 +2,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('Message Received:', message);
     if (message.type === 'ELEMENT_STYLE_CHANGES') {
         console.log('🔄 External CSS Changes Detected:', message.changes);
-        chrome.storage.local.set({ elementCssChanges: message.changes });
+        chrome.storage.local.set({ elementCssChanges: message.changes }, () => {
+            chrome.runtime.sendMessage({ type: 'CSS_CHANGES_UPDATED', changes: message.changes });
+        });
     }
 
     if (message.type === 'STYLESHEET_CSS_CHANGES') {
         console.log('🔄 Inline CSS Change Detected:', message.changes);
-        chrome.storage.local.set({ styleSheetChanges: message.changes });
+        chrome.storage.local.set({ styleSheetChanges: message.changes }, () => {
+            chrome.runtime.sendMessage({ type: 'CSS_CHANGES_UPDATED', changes: message.changes });
+        });
     }
 });
